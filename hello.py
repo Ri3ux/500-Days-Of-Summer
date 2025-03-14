1ac1f2e11246c542fd8117ce149ea9c6abcf9359
Rieux <rieux.bot@gmail.com>
2025-03-14 17:41:23 -0500

    Primera sesión con Brais

---
+++ hello.py
@@ -0,0 +1,15 @@
+# Haremos nuestro primer Hola Mundo en Python
+"""
+Este es un comentario en bloque
+que se extiende por varias líneas
+y no se imprime en la consola
+"""
+variable_string = "Hola Mundo"
+variable_integer = 1
+variable_float = 1.0
+variable_boolean = True
+print(variable_string, variable_integer, variable_float, variable_boolean)
+
+# Variables en una sola línea
+name, surname, alias, age = "Ricardo", "Estepa", "Rieux", 35
+print("Me llamo:", name, surname,". Mi alias es:", alias, ", y tengo ", age, " años.")
