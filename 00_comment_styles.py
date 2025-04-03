a2a7723213f57f7f89d30a0f2430da88ed9acaed
Rieux <rieux.bot@gmail.com>
2025-04-03 18:13:35 -0500

    Comment Styles cheat table

---
+++ 00_comment_styles.py
@@ -0,0 +1,8 @@
+"""
+Comment Styles
+* Important information is highlighted
+! Deprecated method, do not use
+? Should this method be exposed in the public API?
+TODO: refactor this method so that it conforms to the API
+//@param myParam The parameter for this method
+"""
