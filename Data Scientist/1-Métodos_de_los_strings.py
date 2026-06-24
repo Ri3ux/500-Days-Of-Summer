6f5f0b34856ff228d32c41feb58578436a60bab7
Ri3ux <rieux.bot@gmail.com>
2026-06-24 00:19:27 -0500

    Notas del sprint 1 y 2 añadidas

--- Data Scientist/1-Métodos_de_los_strings.py
+++
@@ -1,9 +0,0 @@
-# Métodos de los strings
-upper () # Convierte a mayúsculas
-lower () # Convierte a minúsculas
-capitalize () # Convierte la primera letra a mayúscula
-title () # Convierte la primera letra de cada palabra a mayúscula
-strip () # Elimina espacios al inicio y al final
-replace () # Reemplaza una subcadena por otra
-split () # Divide el string en una lista de subcadenas
-join () # Une una lista de subcadenas en un string
